4d8ac128fddd70db500b9efbf744abed48129c83
Sushant Garde <159456830+sushantgarde@users.noreply.github.com>
2026-08-05 15:17:22 +0530

    0283-move-zeroes 7 ms (39.45%) .py 20.5 MB (24.14%)

---
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -0,0 +1,18 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        j = 0  # position for non-zero
+
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[j] = nums[i]
+                j += 1
+
+        # fill remaining with 0
+        for i in range(j, len(nums)):
+            nums[i] = 0 
+        return nums
+            
+        
