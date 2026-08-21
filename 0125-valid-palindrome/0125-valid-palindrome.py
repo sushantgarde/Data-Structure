bac99f74b083b764556843a9464884452f4b9009
Sushant Garde <159456830+sushantgarde@users.noreply.github.com>
2026-08-21 13:24:12 +0530

    08-21-2026 0125-valid-palindrome .py Easy

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,16 +1,19 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        if len(s) == 0:
-            return True
-        s = s.lower()
-        s = "".join(s.split())
+        # if len(s) == 0:
+        #     return True
+        # s = s.lower()
+        # s = "".join(s.split())
 
-        import re
-        s = re.sub(r'[^a-zA-Z0-9]', '', s)
+        # import re
+        # s = re.sub(r'[^a-zA-Z0-9]', '', s)
 
-        # print(s)
-        # print("Reversed", s[::-1])
-        if s == s[::-1]:
-            return True
-        else:
-            return False
+        # # print(s)
+        # # print("Reversed", s[::-1])
+        # if s == s[::-1]:
+        #     return True
+        # else:
+        #     return False
+        res="".join([char for char in s if char.isalnum()])
+        res=res.lower()
+        return res==res[::-1]
