73e34e900949c5ce2887f515d373e354de7df56b
Sushant Garde <159456830+sushantgarde@users.noreply.github.com>
2026-08-25 21:31:38 +0530

    08-25-2026 0049-group-anagrams .py Medium

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,12 @@
+from collections import defaultdict
+from typing import List
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagram_groups = defaultdict(list)
+        
+        for word in strs:
+            sorted_word = ''.join(sorted(word))
+            anagram_groups[sorted_word].append(word)
+        
+        return list(anagram_groups.values())
