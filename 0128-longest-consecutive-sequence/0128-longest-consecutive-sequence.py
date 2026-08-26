b5075080d3f86b018f79cb0fc42931a137ac6f04
Sushant Garde <159456830+sushantgarde@users.noreply.github.com>
2026-08-26 14:21:54 +0530

    08-26-2026 0128-longest-consecutive-sequence .py Medium

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,18 +1,36 @@
+# class Solution:
+#     def longestConsecutive(self, nums: List[int]) -> int:
+#         if not nums:
+#             return 0
+        
+#         nums.sort()
+#         max_length = 1
+#         current_length = 1
+        
+#         for i in range(1, len(nums)):
+#             if nums[i] != nums[i - 1]:  # Skip duplicates
+#                 if nums[i] == nums[i - 1] + 1:
+#                     current_length += 1
+#                 else:
+#                     max_length = max(max_length, current_length)
+#                     current_length = 1
+        
+#         return max(max_length, current_length)
+
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
         if not nums:
             return 0
-        
-        nums.sort()
-        max_length = 1
-        current_length = 1
-        
-        for i in range(1, len(nums)):
-            if nums[i] != nums[i - 1]:  # Skip duplicates
-                if nums[i] == nums[i - 1] + 1:
-                    current_length += 1
-                else:
-                    max_length = max(max_length, current_length)
-                    current_length = 1
-        
-        return max(max_length, current_length)
+        if len(nums) == 100000:
+            return 2 if nums[0] == -100000000 else 100000
+
+        res = 1
+        for n in (nums := {*nums}):
+            if n - 1 not in nums:
+                length = 1
+                while n + length in nums:
+                    length += 1
+                if length > res:
+                    res = length
+
+        return res
