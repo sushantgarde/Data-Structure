e956eb127f8e04cce007cb5cf65d6c5cdb984e67
Sushant Garde <159456830+sushantgarde@users.noreply.github.com>
2026-08-26 14:15:26 +0530

    08-26-2026 0347-top-k-frequent-elements .py Medium

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -2,12 +2,9 @@ class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         from collections import Counter
         freq = Counter(nums)
-        print(freq)
         freq = sorted(freq, key = lambda x: -freq[x])
-        print(freq)
         result = [0]*k
         for i in range(k):
             result[i] = freq[i]
-            print(result)
 
         return result
