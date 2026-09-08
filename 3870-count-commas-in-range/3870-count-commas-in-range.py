411fef385d29c241708af9e4d7a2ab86dbd91d99
Sushant Garde <159456830+sushantgarde@users.noreply.github.com>
2026-09-08 11:50:23 +0530

    09-08-2026 3870-count-commas-in-range .py Easy

--- 3870-count-commas-in-range/3870-count-commas-in-range.py
+++ 3870-count-commas-in-range/3870-count-commas-in-range.py
@@ -1,7 +1,9 @@
 class Solution:
     def countCommas(self, n: int) -> int:
-        if n < 1000:
-            return 0
-        return (n-999)
+        # if n < 1000:
+        #     return 0
+        # return (n-999)
+
+        return (max(n-999, 0))
 
         
